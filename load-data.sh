6cbdb13f090a12f231bae65f86f10775c92e27a4
CRW <chris.wilhite@datastax.com>
2021-09-23 12:04:33 -0400

    convert data to json

--- load-data.sh
+++
@@ -1,8 +0,0 @@
-./load-table.sh users
-./load-table.sh daily_data_usage
-./load-table.sh accountinfo
-./load-table.sh login
-./load-table.sh consumption
-./load-table.sh ratinggroup
-./load-table.sh subscription
-./load-table.sh charged_usage
